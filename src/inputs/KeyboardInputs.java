166fe666c13b022f7a8d5dde79be0088d4daf3c3
leeminhyuk93 <dlalsgur1003@naver.com>
2023-04-07 15:09:55 +0900

    https://www.youtube.com/watch?v=nuRXTWJ66vc&list=PL4rzdwizLaxYmltJQRjq18a9gsSyEQQ-0&index=6
    tutorial 05 succeded

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -3,6 +3,8 @@ package inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import static Utilz.Constants.Directions.*; 
+
 import main.*;
 
 public class KeyboardInputs implements KeyListener {
@@ -21,23 +23,37 @@ public class KeyboardInputs implements KeyListener {
 		
 		switch(e.getKeyCode()) {
 		case KeyEvent.VK_W:
-			gamePanel.changeYDelta(-5);
+			gamePanel.setDirection(UP);
 			break;
 		case KeyEvent.VK_A:
-			gamePanel.changeXDelta(-5);
+			gamePanel.setDirection(LEFT);
 			break;
 		case KeyEvent.VK_S:
-			gamePanel.changeYDelta(5);
+			gamePanel.setDirection(DOWN);
 			break;
 		case KeyEvent.VK_D:
-			gamePanel.changeXDelta(5);
+			gamePanel.setDirection(RIGHT);
 			break;
-		
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.setMoving(false);
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.setMoving(false);
+			break;
+		case KeyEvent.VK_S:
+			gamePanel.setMoving(false);
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.setMoving(false);
+			break;
+		}
 	}
 
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -10,16 +10,25 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import inputs.*;
 
+import static Utilz.Constants.PlayerConstant.*; // static 선언을 앞에 두면 클래스를 import하는 순간에 데이터를 로드한다.
+import static Utilz.Constants.Directions.*; 
+
 public class GamePanel extends JPanel {
 
 	private MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100;
 	private BufferedImage img;
+	private BufferedImage[][] animations;
+	private int aniTick, aniIndex, aniSpeed = 15;
+	private int playerAction = IDLE;
+	private int playerDir = -1;
+	private boolean moving = false;
 	
 	public GamePanel() {
 		mouseInputs = new MouseInputs(this);
 		
 		importImg();
+		loadAnimations();
 		
 		setPanelSize();
 		addKeyListener(new KeyboardInputs(this));
@@ -28,6 +37,15 @@ public class GamePanel extends JPanel {
 	
 	}
 	
+	private void loadAnimations() {
+		animations = new BufferedImage[9][6];
+		
+		for(int j=0; j<animations.length; j++)
+			for(int i=0; i<animations[j].length; i++)
+				animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
+			
+	}
+
 	private void importImg() {
 		InputStream is = this.getClass().getResourceAsStream("/res/player_sprites.png");
 		try {
@@ -35,6 +53,12 @@ public class GamePanel extends JPanel {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
@@ -47,25 +71,72 @@ public class GamePanel extends JPanel {
 		
 	}
 
-	public void changeXDelta(int value) {
-		this.xDelta += value;
+	public void setDirection(int direction) {
+		this.playerDir = direction;
+		this.moving = true;
 	}
 	
-	public void changeYDelta(int value) {
-		this.yDelta += value;
+	public void setMoving(boolean moving) {
+		this.moving = moving;
 	}
 	
-	public void setRectPos(int x, int y) {
-		this.xDelta = x;
-		this.yDelta = y;
+	private void updateAnimationTick() {
+		/*
+		 * aniSpeed가 30이므로 120프레임/4=30 이니까
+		 * 1초에 4장의 스프라이트가 바뀌게 된다.
+		 */
+		aniTick++;
+		if(aniTick >= aniSpeed) {
+			aniTick = 0;
+			aniIndex++;
+			if(aniIndex >= GetSpriteAmount(playerAction))
+				aniIndex = 0;
+		}
+	}
+	
+	private void setAnimation() {
+		if(moving)
+			playerAction = RUNNING;
+		else
+			playerAction = IDLE;
+	}
+	
+	private void updatePos() {
+		if(moving) {
+			switch(playerDir) {
+			case LEFT:
+				xDelta -= 5;
+				break;
+			case UP:
+				yDelta -= 5;
+				break;
+			case RIGHT:
+				xDelta += 5;
+				break;
+			case DOWN:
+				yDelta += 5;
+				break;
+			}
+		}
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		//img = 원본, subImg = 원본에서 가져온 각각의 이미지
+		updateAnimationTick();
 		
-//		g.drawImage(null, x, y, null);
+		setAnimation();
+		updatePos();
+		
+		g.drawImage(animations[playerAction][aniIndex], (int)xDelta, (int)yDelta, 256, 160, null);
 	}
+
+	
+
+	
+
+	
 	
 }
 
